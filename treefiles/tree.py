ebd09449d2c19bac46dc76eb486a2bec026f487e
Emmanuel Arias <eamanu@yaerobi.com>
2020-12-25 22:37:36 -0300

    Revert "oops tree"
    This reverts commit 919cf59ed43b080722f0d1791f20e5b10eca1565.

--- treefiles/tree.py
+++ treefiles/tree.py
@@ -3,18 +3,19 @@ import pathlib
 from treefiles.utils import get_simulate_folder
 from treefiles.drawer import Drawer
 
-class Tre:
+class Tree:
     def __init__(self):
-        """No hacer nada aca, todo en __call__"""
         self.fmt = ''
 
-    def __call___(self.path, fmt):
+    def __call__(self, path, fmt):
         """este metodo hace que se imprima el path en un formato determinado
 
         Se debe capturar los errores que puedan ocurrir e imprimir
         un mensaje de error.
         """
         self.fmt = fmt
+        d = Drawer(self.get_directories_structure(path), fmt)
+        d.print_tree()
 
     def get_directories_structure(self, path):
         # implement this
